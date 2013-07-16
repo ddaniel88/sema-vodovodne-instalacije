b3e7ab7abc875b6d9bb87d7a4d454598e938f9d2
Daniel Dimitrov <fap.13.fap@gmail.com>
2013-07-16 23:39:51 +0200

    djole

--- Sema vodovodne instalacije/src/MovingRectangle.java
+++ Sema vodovodne instalacije/src/MovingRectangle.java
@@ -1,41 +1,23 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Cursor;
-import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.List;
 import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
 import java.awt.geom.AffineTransform;
-import java.awt.geom.Ellipse2D;
-import java.awt.geom.Line2D;
-import java.awt.geom.Rectangle2D;
-import java.io.Console;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
-
-import javax.swing.ImageIcon;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
 import javax.swing.JSeparator;
-import javax.swing.JViewport;
-import javax.swing.SwingUtilities;
-
 import enums.ScaleEnum;
 import figures.E_Spn;
 import figures.Figure;
@@ -43,7 +25,6 @@ import figures.Point;
 
 public class MovingRectangle extends JFrame {
 	public static TransformingCanvas canvas;
-	private static Kvadrat kvadrat;
 	static Cursor curCursor;
 	private static E_Spn espn, sspn,espn2;
 	private static ArrayList<Figure> figure;
@@ -54,19 +35,23 @@ public class MovingRectangle extends JFrame {
 	public static class MenuActionListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 
-			System.out.println("Selected: " + e.getActionCommand());
-
 			String figura;
 
 			switch (e.getActionCommand()) {
 			case "001":
-				System.out.println("aaa");
+				espn = new E_Spn(200, 200, 100, 50);
+				espn.setDescription("Prva figura");
+				figure.add(espn);
 				break;
 			case "002":
-				System.out.println("bbb");
+				sspn = new E_Spn(10, 10, 40, 10);
+				sspn.setDescription("Mala figura");
+				figure.add(sspn);
 				break;
 			case "003":
-				System.out.println("ccc");
+				espn2 = new E_Spn(100, 50, 40, 20);
+				espn2.setDescription("Treca figura");
+				figure.add(espn2);
 				break;
 
 			default:
@@ -154,11 +139,10 @@ public class MovingRectangle extends JFrame {
 			addMouseListener(ma);
 			addMouseWheelListener(ma);
 
-			kvadrat = new Kvadrat(70, 70, 20, 20);
 			// zell = new ZEllipse(150, 70, 80, 80);
 
 			/******** DRAW CUSTOM FIGURES ********/
-			espn = new E_Spn(200, 200, 100, 50);
+		/*	espn = new E_Spn(200, 200, 100, 50);
 			espn.setDescription("Prva figura");
 			figure.add(espn);
 
@@ -169,6 +153,7 @@ public class MovingRectangle extends JFrame {
 			espn2 = new E_Spn(100, 50, 40, 20);
 			espn2.setDescription("Treca figura");
 			figure.add(espn2);
+			*/
 			/******** END OF DRAW CUSTOM FIGURES ********/
 
 			setOpaque(true);
@@ -188,16 +173,9 @@ public class MovingRectangle extends JFrame {
 					RenderingHints.VALUE_ANTIALIAS_ON);
 			ourGraphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
 					RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-			ourGraphics.setColor(Color.BLACK);
-			ourGraphics.drawString("Proba zoom-in i zoom-out", 100, 100);
+			ourGraphics.setColor(Color.BLUE);
 
-			AffineTransform aa = new AffineTransform();
-			aa.scale(kvadrat.kv_scale, kvadrat.kv_scale);
-			Graphics2D kv = (Graphics2D) g;
-			kv.setColor(new Color(0, 0, 200));
-			kv.setTransform(aa);
-			kv.fill(kvadrat);
-
+		
 			drawAllFigures(ourGraphics);
 
 			if (curCursor != null) {
@@ -234,47 +212,7 @@ public class MovingRectangle extends JFrame {
 		}
 	}
 
-	public static class Kvadrat extends Rectangle2D.Float {
-
-		double kv_scale = 1;
-
-		public Kvadrat(double d, double e, float width, float height) {
-			setRect(d, e, width, height);
-		}
-
-		public boolean isHit(float x, float y) {
-
-			if (getBounds2D().contains(x, y)) {
-				return true;
-			} else {
-				return false;
-			}
-		}
-
-		public void addX(float x) {
-			this.x += x;
-		}
-
-		public void addY(float y) {
-			this.y += y;
-		}
-
-		public void addWidth(float w) {
-			this.width += w;
-		}
-
-		public void addHeight(float h) {
-			this.height += h;
-		}
-	}
-
-	public static class Nesto extends Line2D.Float {
-		private static final long serialVersionUID = 1L;
-
-		public Nesto(float a, float b, float c, float d) {
-			setLine(a, b, c, d);
-		}
-	}
+	
 
 	public static class MovingAdapter extends MouseAdapter {
 		private int x;
@@ -297,10 +235,6 @@ public class MovingRectangle extends JFrame {
 			int dx = e.getX() - x;
 			int dy = e.getY() - y;
 
-			if (kvadrat.isHit(x, y)) {
-				kvadrat.addX(dx);
-				kvadrat.addY(dy);
-			}
 
 			if (tmpFigure != null) {
 				tmpFigure.moveFigureFor(dx, dy, endCanvas());
@@ -321,17 +255,6 @@ public class MovingRectangle extends JFrame {
 				} else {
 					curCursor = Cursor.getDefaultCursor();
 				}
-				
-				/*else {
-					if (kvadrat != null) {
-						if (kvadrat.contains(e.getX(), e.getY())) {
-							curCursor = Cursor
-									.getPredefinedCursor(Cursor.HAND_CURSOR);
-						} else {
-							curCursor = Cursor.getDefaultCursor();
-						}
-					}
-				}*/
 			}
 			canvas.repaint();
 		}
@@ -353,36 +276,6 @@ public class MovingRectangle extends JFrame {
 				}
 			}
 
-			// System.out.println("mouseWheelMoved " + kvadrat.getX() + ", "
-			// + kvadrat.getY());
-			//
-			// // kvadrat.setRect(kvadrat.x *= (1 - .1 * e.getWheelRotation()),
-			// kvadrat.y *= (1 - .1 * e.getWheelRotation()),
-			// // kvadrat.width *= (1 - .1 * e.getWheelRotation()),
-			// kvadrat.height *= (1 - .1 * e.getWheelRotation()));
-			//
-			// // minus ide zato što je obrnuto kolce na dole i kolce na gore
-			// :-)
-			// // promenimo kvadrat pravougaonika i faktièki smo ga zumirali
-			// double newX = kvadrat.getX() - kvadrat.getX() *
-			// e.getWheelRotation() / 10;
-			// double newY = kvadrat.getY() - kvadrat.getY() *
-			// e.getWheelRotation() / 10;
-			// double newWidth = kvadrat.getWidth() - kvadrat.getWidth() *
-			// e.getWheelRotation() / 10;
-			// double newHeight = kvadrat.getHeight() - kvadrat.getHeight() *
-			// e.getWheelRotation() / 10;
-			//
-			// kvadrat.setRect(newX, newY, newWidth, newHeight);
-			//
-			// // make it a reasonable amount of zoom
-			// // .1 gives a nice slow transition
-			// // kvadrat.kv_scale -= (.1 * e.getWheelRotation());
-			// // don't cross negative threshold.
-			// // also, setting scale to 0 has bad effects
-			// // kvadrat.kv_scale = Math.max(0.00001, kvadrat.kv_scale);
-			//
-			// System.out.println("Trenutni zoom: " + kvadrat.kv_scale);
 			canvas.repaint();
 		}
 
