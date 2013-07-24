185fd7b1dccd48195bbe64e6b04a6af9e5712761
manoilov88 <manoilov88@gmail.com>
2013-07-24 19:23:26 +0200

    sema-vodovodne-instalacije/issues/12
    Dodati su eventi na scroll, poziva se AdjustmentEvent.TRACK kada se
    skroluje

--- Sema vodovodne instalacije/src/MovingRectangle.java
+++ Sema vodovodne instalacije/src/MovingRectangle.java
@@ -1,3 +1,4 @@
+import java.awt.Adjustable;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Cursor;
@@ -8,6 +9,8 @@ import java.awt.RenderingHints;
 import java.awt.ScrollPane;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseWheelEvent;
@@ -177,8 +180,49 @@ public class MovingRectangle extends JFrame {
         
         frame.setSize(400,400);  
         
+     // Listen for value changes in the scroll pane's scrollbars
+        AdjustmentListener listener = new MyAdjustmentListener();
+        scrollPane.getHorizontalScrollBar().addAdjustmentListener(listener);
+        scrollPane.getVerticalScrollBar().addAdjustmentListener(listener);
        
 	}
+	
+	
+	public static class MyAdjustmentListener implements AdjustmentListener {
+		  public void adjustmentValueChanged(AdjustmentEvent evt) {
+		    Adjustable source = evt.getAdjustable();
+		    if (evt.getValueIsAdjusting()) {
+		      return;
+		    }
+		    int orient = source.getOrientation();
+		    if (orient == Adjustable.HORIZONTAL) {
+		      System.out.println("from horizontal scrollbar"); 
+		    } else {
+		      System.out.println("from vertical scrollbar");
+		    }
+		    int type = evt.getAdjustmentType();
+		    switch (type) {
+		    case AdjustmentEvent.UNIT_INCREMENT:
+		      System.out.println("Scrollbar was increased by one unit");
+		      break;
+		    case AdjustmentEvent.UNIT_DECREMENT:
+		      System.out.println("Scrollbar was decreased by one unit");
+		      break;
+		    case AdjustmentEvent.BLOCK_INCREMENT:
+		      System.out.println("Scrollbar was increased by one block");
+		      break;
+		    case AdjustmentEvent.BLOCK_DECREMENT:
+		      System.out.println("Scrollbar was decreased by one block");
+		      break;
+		    case AdjustmentEvent.TRACK:
+		      System.out.println("The knob on the scrollbar was dragged");
+		      break;
+		    }
+		    int value = evt.getValue();
+		  }
+
+		}
+	
 
 	public static class TransformingCanvas extends JPanel {
 		private double translateX;
